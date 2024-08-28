dbed15744d9a7e2f67cfd0afd5419537fc723641
Alejandro García Navia <alejogarcia@unicauca.edu.co>
2024-08-28 01:02:59 +0000

    Excuse generator with Bootstrap without stylesheet finalized

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function(){
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    let randomwho=Math.floor(Math.random()*who.length);
+    let randomaction=Math.floor(Math.random()*action.length);
+    let randomwhat=Math.floor(Math.random()*what.length);
+    let randomwhen=Math.floor(Math.random()*when.length);
+
+    document.querySelector("#excuse").innerHTML= who[randomwho]+ " " + action [randomaction] + " " + what [randomwhat] + " " + when [randomwhen];
 };
